49c849a6478342aef2fa9ae73609af66d19f8bdc
Pedram Veisi <pedramveisi@gmail.com>
2015-11-09 18:25:43 -0700

    Implement events listview basics

---
+++ GoodIntents/app/src/main/java/com/udacity/adcs/app/goodintents/ui/list/ActivityListAdapter.java
@@ -0,0 +1,46 @@
+package com.udacity.adcs.app.goodintents.ui.list;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by pedram on 09/11/15.
+ */
+public class ActivityListAdapter extends ArrayAdapter<String> {
+
+    private Context mContext;
+
+    /**
+     * Constructor
+     *
+     * @param context  The current context.
+     * @param resource The resource ID for a layout file containing a TextView to use when
+     */
+    public ActivityListAdapter(Context context, int resource) {
+        super(context, resource);
+        this.mContext = context;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return super.getView(position, convertView, parent);
+    }
+
+    /**
+     * View holder for activity list adapter
+     */
+    static class ViewHolder {
+        TextView activityNameTextView;
+        TextView activtyDateTextView;
+        TextView pointsTextView;
+        ImageView pointsIconImageView;
+    }
+
+}
