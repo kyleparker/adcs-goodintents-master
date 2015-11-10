63f9ba74ef35aff3d386ba14e12e8a314d0150ed
Pedram Veisi <pedramveisi@gmail.com>
2015-11-09 19:05:23 -0700

    Implement events listview with basic design and dummy data

--- GoodIntents/app/src/main/java/com/udacity/adcs/app/goodintents/ui/fragment/ProfileFragment.java
+++ GoodIntents/app/src/main/java/com/udacity/adcs/app/goodintents/ui/fragment/ProfileFragment.java
@@ -5,10 +5,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.udacity.adcs.app.goodintents.R;
+import com.udacity.adcs.app.goodintents.objects.Event;
 import com.udacity.adcs.app.goodintents.ui.base.BaseFragment;
+import com.udacity.adcs.app.goodintents.ui.list.EventListAdapter;
+
+import java.util.ArrayList;
 
 /**
  * Created by kyleparker on 11/9/2015.
@@ -44,6 +49,25 @@ public class ProfileFragment extends BaseFragment {
         profilePicImageView.setImageResource(R.mipmap.ic_launcher);
         nameTextView.setText("Test");
         extraInfo.setText("Test Location");
+
+        // Dummy Data
+        ArrayList<Event> eventsList = new ArrayList<>();
+        Event event = new Event();
+        event.setName("Test Event");
+        event.setDate(123456);
+        event.setOrganization("Test Organization");
+
+        eventsList.add(event);
+        eventsList.add(event);
+        eventsList.add(event);
+        eventsList.add(event);
+        eventsList.add(event);
+        eventsList.add(event);
+
+
+        ListView eventsListView = (ListView) mRootView.findViewById(R.id.events_list_view);
+        EventListAdapter eventListAdapter = new EventListAdapter(getActivity(), R.layout.list_item_event, eventsList);
+        eventsListView.setAdapter(eventListAdapter);
 
         return mRootView;
     }
