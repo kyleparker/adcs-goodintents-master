aede62fe9d505837bec48b9e757d45e684cd3113
Pedram Veisi <pedramveisi@gmail.com>
2015-11-09 18:44:30 -0700

    Implement basic event list

--- GoodIntents/app/src/main/java/com/udacity/adcs/app/goodintents/ui/list/EventListAdapter.java
+++ GoodIntents/app/src/main/java/com/udacity/adcs/app/goodintents/ui/list/EventListAdapter.java
@@ -2,20 +2,25 @@ package com.udacity.adcs.app.goodintents.ui.list;
 
 import android.app.Activity;
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.udacity.adcs.app.goodintents.R;
+import com.udacity.adcs.app.goodintents.objects.Event;
+
 import java.util.ArrayList;
 
 /**
  * Created by pedram on 09/11/15.
  */
-public class EventListAdapter extends ArrayAdapter<String> {
+public class EventListAdapter extends ArrayAdapter<Event> {
 
     private Context mContext;
+    private ArrayList<Event> mListItems;
 
     /**
      * Constructor
@@ -23,14 +28,39 @@ public class EventListAdapter extends ArrayAdapter<String> {
      * @param context  The current context.
      * @param resource The resource ID for a layout file containing a TextView to use when
      */
-    public EventListAdapter(Context context, int resource) {
-        super(context, resource);
+    public EventListAdapter(Context context, int resource, ArrayList<Event> listItems) {
+        super(context, resource, listItems);
         this.mContext = context;
+        this.mListItems = listItems;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return super.getView(position, convertView, parent);
+
+        if(convertView == null){
+            LayoutInflater inflater = (LayoutInflater) mContext
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.list_item_event, parent, false);
+
+            // Setup view holder
+            ViewHolder viewHolder = new ViewHolder();
+            viewHolder.eventNameTextView = (TextView) convertView.findViewById(R.id.event_name_textview);
+            viewHolder.eventDateTextView = (TextView) convertView.findViewById(R.id.event_date_textview);
+            viewHolder.organizationTextView = (TextView) convertView.findViewById(R.id.organization_text_view);
+            convertView.setTag(viewHolder);
+        }
+
+        ViewHolder viewHolder = (ViewHolder) convertView.getTag();
+        Event event = mListItems.get(position);
+
+        viewHolder.eventNameTextView.setText(event.getName());
+
+        // TODO Convert date to a human readable string
+        viewHolder.eventDateTextView.setText(Long.toString(event.getDate()));
+
+        viewHolder.organizationTextView.setText(event.getOrganization());
+
+        return convertView;
     }
 
     /**
@@ -39,8 +69,7 @@ public class EventListAdapter extends ArrayAdapter<String> {
     static class ViewHolder {
         TextView eventNameTextView;
         TextView eventDateTextView;
-        TextView pointsTextView;
-        ImageView pointsIconImageView;
+        TextView organizationTextView;
     }
 
 }
