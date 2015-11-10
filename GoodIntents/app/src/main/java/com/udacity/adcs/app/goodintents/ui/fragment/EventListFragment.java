9c2d4cfeb427df3729222629b0db8173bb31d413
Perk <perklun@gmail.com>
2015-11-10 15:29:43 -0800

    finished recyclerview

--- GoodIntents/app/src/main/java/com/udacity/adcs/app/goodintents/ui/fragment/EventListFragment.java
+++ GoodIntents/app/src/main/java/com/udacity/adcs/app/goodintents/ui/fragment/EventListFragment.java
@@ -9,16 +9,20 @@ import android.view.ViewGroup;
 
 import com.udacity.adcs.app.goodintents.R;
 import com.udacity.adcs.app.goodintents.objects.Event;
+import com.udacity.adcs.app.goodintents.objects.PersonEvent;
 import com.udacity.adcs.app.goodintents.ui.base.BaseFragment;
 import com.udacity.adcs.app.goodintents.ui.list.EventListRecyclerViewAdapter;
+import com.udacity.adcs.app.goodintents.ui.list.PersonEventListRecyclerViewAdapter;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by kyleparker on 11/9/2015.
  */
 public class EventListFragment extends BaseFragment {
-
+    private List<Event> mEventList;
+    private EventListRecyclerViewAdapter mPersonEventListRecyclerViewAdapter;
     /**
      * Factory method to generate a new instance of the fragment
      *
@@ -43,9 +47,32 @@ public class EventListFragment extends BaseFragment {
         RecyclerView rv_event_list = (RecyclerView) mRootView.findViewById(R.id.rv_event_list);
         RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(mActivity);
         rv_event_list.setLayoutManager(mLayoutManager);
-
-        EventListRecyclerViewAdapter rv_adapter = new EventListRecyclerViewAdapter(new ArrayList<Event>());
-        rv_event_list.setAdapter(rv_adapter);
+        mPersonEventListRecyclerViewAdapter = new EventListRecyclerViewAdapter(new ArrayList<Event>());
+        rv_event_list.setAdapter(mPersonEventListRecyclerViewAdapter);
+        getEventList();
         return mRootView;
     }
+
+    public void getEventList() {
+        Runnable load = new Runnable() {
+            public void run() {
+                try {
+                    mEventList = mProvider.getEventList();
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                } finally {
+                    mActivity.runOnUiThread(getEventListRunnable);
+                }
+            }
+        };
+        Thread thread = new Thread(null, load, "getEventList");
+        thread.start();
+    }
+
+    private final Runnable getEventListRunnable = new Runnable() {
+        public void run() {
+            mPersonEventListRecyclerViewAdapter.setEventList(mEventList);
+            mPersonEventListRecyclerViewAdapter.notifyDataSetChanged();
+        }
+    };
 }
