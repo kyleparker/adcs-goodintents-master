a66f528700da54c704bdcd6e0b7732e34aaf022e
Perk <perklun@gmail.com>
2015-11-10 09:49:19 -0800

    Added RecyclerView for friendlist

--- GoodIntents/app/src/main/java/com/udacity/adcs/app/goodintents/utils/EventListRecyclerViewAdapter.java
+++ GoodIntents/app/src/main/java/com/udacity/adcs/app/goodintents/utils/EventListRecyclerViewAdapter.java
@@ -4,6 +4,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.udacity.adcs.app.goodintents.R;
@@ -23,11 +24,13 @@ public class EventListRecyclerViewAdapter extends RecyclerView.Adapter<EventList
         public TextView tv_event_title;
         public TextView tv_event_date;
         public TextView tv_event_details;
+        public ImageView iv_event_image;
         public ViewHolder(View v) {
             super(v);
             tv_event_date = (TextView) v.findViewById(R.id.tv_event_date);
             tv_event_details = (TextView) v.findViewById(R.id.tv_event_details);
             tv_event_title = (TextView) v.findViewById(R.id.tv_event_title);
+            iv_event_image = (ImageView) v.findViewById(R.id.iv_event_image);
         }
     }
 
@@ -49,6 +52,7 @@ public class EventListRecyclerViewAdapter extends RecyclerView.Adapter<EventList
         holder.tv_event_date.setText("11/11");
         holder.tv_event_details.setText("Fun run");
         holder.tv_event_title.setText("Awesome event");
+        holder.iv_event_image.setImageResource(R.mipmap.ic_launcher);
     }
 
     @Override
